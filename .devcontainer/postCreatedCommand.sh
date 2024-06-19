97d3edf1f99cfa4397566c2a1ef685c9704ee262
bizhermit <rsenda@bizhermit.com>
2024-06-19 16:36:20 +0900

    feat: add devcontainer settings

---
+++ .devcontainer/postCreatedCommand.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "node" | sudo -S chown node /workspace/node_modules && sudo -S chmod 777 /workspace/node_modules
+echo "node" | sudo -S chown node /workspace/.next && sudo -S chmod 777 /workspace/.next
+echo "node" | sudo -S chown node /workspace/.server && sudo -S chmod 777 /workspace/.server
