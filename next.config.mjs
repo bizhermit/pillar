8b7f2df555ccb63a8685493827513f5eced2d937
bizhermit <rsenda@bizhermit.com>
2024-06-19 19:04:31 +0900

    feat: next scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 const appMode = process.env.APP_MODE;
+const outputMode = process.env.NEXT_OUTPUT;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     tsconfigPath: './src/tsconfig.json',
   },
+  ...(outputMode === 'dist' ? {
+    output: 'export',
+    distDir: 'dist/out',
+    trailingSlash: true,
+  } : {}),
+  ...(outputMode === 'renderer' ? {
+    output: 'export',
+    distDir: '.renderer',
+  } : {}),
   pageExtensions: [
     'tsx', 'ts', 'mts', 'cts',
     ...(appMode === 'dev' ? ['dev.tsx', 'dev.ts', 'dev.mts', 'dev.cts'] : [])
