ce01fc34ed86f0e9a33322d5ad7dc6eec7a4de44
bizhermit <rsenda@bizhermit.com>
2024-06-26 12:19:50 +0900

    feat: サンプルAPI

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,17 @@
+import { $bool } from "@/data-items/bool";
+import { $num } from "@/data-items/number";
+import { $str } from "@/data-items/string";
 import { apiMethodHandler } from "@/server/api-handler/app-api";
 
-export const GET = apiMethodHandler(async () => {
-
+export const GET = apiMethodHandler(async (props) => {
+  const data = await props.getParams([
+    $str({ name: "hoge", required: true }),
+    $num({ name: "fuga", required: false }),
+    $bool({ name: "piyo", required: true }),
+  ]);
+  console.log(data);
+  props.throwIfHasValidationError();
+  return {
+    req: data,
+  };
 });
