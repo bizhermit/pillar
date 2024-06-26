92d1bb8c8e9e93cea18e7b1db268f2fcc37950e8
bizhermit <rsenda@bizhermit.com>
2024-06-26 09:27:35 +0900

    feat: file parse

---
+++ src/components/data-items/file/parse.ts
@@ -0,0 +1,27 @@
+import { convertBase64ToFile, convertBlobToFile } from "@/objects/file";
+
+const defaultLabel = "値";
+
+export const $fileParse = (value: any, dataItem: DataItem.$file): DataItem.ParseResult<File> => {
+  if (value == null || value === "") return [undefined];
+
+  const label = dataItem.label || defaultLabel;
+  try {
+    if (value instanceof File) return [value];
+    if (value instanceof Blob) {
+      return [
+        convertBlobToFile(value, dataItem.fileName || dataItem.name),
+        { type: "i", code: "parse", msg: `${label}をファイル型に変換しました。[blob]` },
+      ];
+    }
+    if (typeof value === "string") {
+      return [
+        convertBase64ToFile(value, dataItem.fileName || dataItem.name, { type: dataItem.type }),
+        { type: "i", code: "parse", msg: `${label}をファイル型に変換しました。[base64]` },
+      ];
+    }
+    throw new Error;
+  } catch {
+    return [undefined, { type: "e", code: "parse", msg: `${label}をファイル型に変換できません。` }];
+  }
+};
