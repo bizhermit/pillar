f857422ce8144df857f35acc2fa2d83111426161
bizhermit <rsenda@bizhermit.com>
2024-07-03 12:14:38 +0900

    feat: form

---
+++ src/components/react/elements/form/index.tsx
@@ -0,0 +1,294 @@
+import { createContext, useEffect, useMemo, useReducer, useRef, type Dispatch, type FormEvent, type FormHTMLAttributes, type MutableRefObject } from "react";
+import { clone } from "../../../objects";
+import { getValue, setValue } from "../../../objects/struct";
+import { useRefState } from "../../hooks/ref-state";
+
+type FormError = {
+  id: string;
+  content: DataItem.ValidationResult | null | undefined;
+};
+
+type FormItemMountProps = {
+  id: string;
+  name: string;
+  get: <T>() => T;
+  set: (value: any) => void;
+  reset: () => void;
+};
+
+type FormState = "init" | "submit" | "reset" | null | "nothing";
+
+type FormContextProps = {
+  bind: { [v: string]: any };
+  disabled?: boolean;
+  state: FormState;
+  method?: string;
+  hasError: boolean;
+  setItemState: Dispatch<FormError>;
+  mount: (props: FormItemMountProps) => {
+    unmount: () => void;
+  };
+  getValue: <T>(name: string) => T;
+  setValue: (name: string, value: any) => void;
+};
+
+export const FormContext = createContext<FormContextProps>({
+  bind: {},
+  disabled: false,
+  state: "nothing",
+  hasError: false,
+  setItemState: () => { },
+  mount: () => {
+    throw new Error("no form");
+  },
+  getValue: () => undefined as any,
+  setValue: () => {},
+});
+
+type GetBindDataOptions = {
+  removeNotMounted?: boolean;
+};
+
+type FormRef<T extends { [v: string]: any } = { [v: string]: any }> = {
+  getElement: () => (HTMLFormElement | null);
+  submit: () => void;
+  reset: () => void;
+  getFormData: () => FormData;
+  getBindData: (options?: GetBindDataOptions) => T;
+  getValue: <T>(name: string) => T;
+  setValue: (name: string, value: any) => void;
+};
+
+type FormOptions<T extends { [v: string]: any } = { [v: string]: any }> = {
+  ref?: MutableRefObject<FormRef<T> | null>;
+  encType?: "application/x-www-form-urlencoded" | "multipart/form-data" | "text/plain";
+  disabled?: boolean;
+  bind?: { [v: string | number | symbol]: any };
+  preventEnterSubmit?: boolean;
+  onSubmit?: ((props: {
+    event: FormEvent<HTMLFormElement>;
+    getFormData: () => FormData;
+    getBindData: (options?: GetBindDataOptions) => T;
+    keepLock: () => () => void;
+  }) => (void | boolean | Promise<void>)) | boolean;
+  onReset?: ((props: {
+    event: FormEvent<HTMLFormElement>;
+    getFormData: () => FormData;
+    getBindData: (options?: GetBindDataOptions) => T;
+  }) => (void | boolean | Promise<void | boolean>)) | boolean;
+};
+
+type FormProps<T extends { [v: string]: any } = { [v: string]: any }> = OverwriteAttrs<HTMLFormElement, FormHTMLAttributes<HTMLFormElement>, FormOptions<T>>;
+
+export const Form = <T extends { [v: string]: any } = { [v: string]: any }>({
+  ref,
+  disabled,
+  bind,
+  preventEnterSubmit,
+  onSubmit,
+  onReset,
+  ...props
+}: FormProps<T>) => {
+  const $ref = useRef<HTMLFormElement>(null);
+  const $bind = useMemo(() => {
+    if (bind == null) return {};
+    return bind;
+  }, [bind]);
+
+  const [formState, setFormState, formStateRef] = useRefState<FormState>("init");
+
+  const items = useRef<{ [id: string]: FormItemMountProps }>({});
+  const findItem = (name: string) => {
+    const id = Object.keys(items.current).find(id => items.current[id].name === name);
+    if (id == null) return undefined;
+    return items.current[id];
+  };
+
+  const [itemState, setItemState] = useReducer((state: { [id: string]: Exclude<FormError["content"], null | undefined> }, { id, content }: FormError) => {
+    const buf = state[id];
+    if (content == null) {
+      if (buf == null) return state;
+      const ret = { ...state };
+      delete ret[id];
+      return ret;
+    }
+    if (buf == null) {
+      return {
+        ...state,
+        [id]: content,
+      };
+    }
+    if (buf.type === content.type && buf.msg === content.msg) return state;
+    return {
+      ...state,
+      [id]: content,
+    };
+  }, {});
+
+  const hasError = useMemo(() => {
+    return Object.keys(itemState).some(k => itemState[k].type === "e");
+  }, [itemState]);
+
+  const getFormData = () => new FormData($ref.current!);
+
+  const getBindData = (opts?: GetBindDataOptions) => {
+    if (!opts?.removeNotMounted) return clone($bind);
+    const ret = {};
+    Object.keys(items.current).forEach(id => {
+      const { name } = items.current[id];
+      if (!name) return;
+      setValue(ret, name, clone(getValue($bind, name)));
+    });
+    return ret as any;
+  };
+
+  const $get = (name: string) => findItem(name)?.get<any>();
+
+  const $set = (name: string, value: any) => findItem(name)?.set(value);
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.stopPropagation();
+    if (disabled || formStateRef.current || hasError) {
+      e.preventDefault();
+      return;
+    }
+    setFormState("submit");
+    if (onSubmit === false) {
+      e.preventDefault();
+      setFormState(null);
+      return;
+    }
+    if (onSubmit == null || onSubmit === true) return;
+
+    try {
+      let keepLock = false;
+      const ret = onSubmit({
+        event: e,
+        keepLock: () => {
+          keepLock = true;
+          return () => setFormState(null);
+        },
+        getFormData,
+        getBindData,
+      });
+      if (ret == null || ret === false) {
+        e.preventDefault();
+        if (!keepLock) setFormState(null);
+        return;
+      }
+      if (ret === true) return;
+      e.preventDefault();
+      if (ret instanceof Promise) {
+        ret.finally(() => {
+          if (!keepLock) setFormState(null);
+        });
+      }
+    } catch {
+      e.preventDefault();
+      setFormState(null);
+    }
+  };
+
+  const resetItems = (callback?: () => void) => {
+    Object.keys(items.current).forEach(id => {
+      items.current[id].reset();
+    });
+    callback?.();
+  };
+
+  const reset = (e: FormEvent<HTMLFormElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (disabled || formStateRef.current) return;
+    setFormState("reset");
+    if (onReset == null || onReset === true) {
+      resetItems(() => setFormState(null));
+      return;
+    }
+    if (onReset === false) {
+      setFormState(null);
+      return;
+    }
+
+    try {
+      const ret = onReset({
+        event: e,
+        getFormData,
+        getBindData,
+      });
+      if (ret == null || ret === true) {
+        resetItems(() => setFormState(null));
+        return;
+      }
+      if (ret === false) {
+        setFormState(null);
+        return;
+      }
+      if (ret instanceof Promise) {
+        ret
+          .then((r) => {
+            if (r === false) {
+              setFormState(null);
+              return;
+            }
+            resetItems(() => setFormState(null));
+          }).catch(() => {
+            setFormState(null);
+          });
+      }
+    } catch {
+      setFormState(null);
+    }
+  };
+
+  if (ref) {
+    ref.current = {
+      getElement: () => $ref.current,
+      submit: () => $ref.current?.submit(),
+      reset: () => $ref.current?.reset(),
+      getFormData,
+      getBindData,
+      getValue: $get,
+      setValue: $set,
+    };
+  }
+
+  useEffect(() => {
+    setFormState(null);
+  }, []);
+
+  return (
+    <FormContext.Provider value={{
+      method: props.method,
+      bind: $bind,
+      disabled,
+      state: formState,
+      hasError,
+      mount: (p) => {
+        items.current[p.id] = p;
+        return {
+          unmount: () => {
+            delete items.current[p.id];
+            setItemState({ id: p.id, content: null });
+          },
+        };
+      },
+      setItemState,
+      getValue: $get,
+      setValue: $set,
+    }}>
+      <form
+        {...props}
+        ref={$ref}
+        onSubmit={submit}
+        onReset={reset}
+        onKeyDown={preventEnterSubmit ? (e) => {
+          if (e.key === "Enter" && (e.target as HTMLElement).tagName !== "BUTTON") {
+            e.preventDefault();
+          }
+          props.onKeyDown?.(e);
+        } : props.onKeyDown}
+      />
+    </FormContext.Provider>
+  );
+};
--- src/components/react/elements/type.d.ts
+++ src/components/react/elements/type.d.ts
@@ -1,3 +1,3 @@
-type OverwriteAttrs<E, T extends React.HTMLAttributes<E>, U> = Omit<T, keyof U> & U & {
+type OverwriteAttrs<E, T extends React.HTMLAttributes<E>, U> = Omit<T & {
   ref?: React.MutableRefObject<E | null>;
-};
+}, keyof U> & U;
