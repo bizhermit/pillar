75cd955d66f57cc4b9939cb968d8914790b72d59
bizhermit <rsenda@bizhermit.com>
2024-07-11 09:55:27 +0900

    fix: useRefState refに即時反映

--- src/components/react/hooks/ref-state.ts
+++ src/components/react/hooks/ref-state.ts
@@ -1,7 +1,8 @@
-import { useReducer, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export const useRefState = <T>(init: T) => {
   const r = useRef<T>(init);
-  const [s, set] = useReducer((_: T, a: T) => r.current = a, r.current);
+  const [s, $s] = useState(init);
+  const set = useCallback((v: T) => $s(r.current = v), []);
   return [s, set, r] as const;
 };
