84f6913a8003161da81520f11305f9bf013a9170
bizhermit <rsenda@bizhermit.com>
2024-07-16 09:49:19 +0900

    fix: joinClassNames trim

--- src/components/react/elements/utilities.ts
+++ src/components/react/elements/utilities.ts
@@ -1,1 +1,1 @@
-export const joinClassNames = (...classNames: Array<string | null | undefined>) => classNames.join(" ").replace(/[ ]{2,}/g, " ") || undefined;
+export const joinClassNames = (...classNames: Array<string | null | undefined>) => classNames.join(" ").replace(/[ ]{2,}/g, " ").trim() || undefined;
