4c2f61a0ba1db082647882e21489b379bc0e75fa
bizhermit <rsenda@bizhermit.com>
2024-09-05 14:45:30 +0900

    feat: nav control funcs

--- src/components/react/elements/navigation/client-components.tsx
+++ src/components/react/elements/navigation/client-components.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { MenuLeftRightIcon } from "../icon";
-import { navToggleRadioName } from "./consts";
+import { navOpenId, navToggleRadioName } from "./consts";
+
+export const closeNav = () => {
+  const elem = document.querySelector(`#${navOpenId}`) as HTMLInputElement;
+  if (elem) elem.checked = false;
+};
+
+export const autoNav = () => {
+  document.querySelectorAll(`input[name="${navToggleRadioName}"]`).forEach(elem => {
+    (elem as HTMLInputElement).checked = false;
+  });
+};
 
 export const NavSizeAutoButton = () => {
   return (
     <div
       className="nav-btn nav-auto"
-      onClick={() => {
-        document.querySelectorAll(`input[name="${navToggleRadioName}"]`).forEach(elem => {
-          (elem as HTMLInputElement).checked = false;
-        });
-      }}
+      onClick={autoNav}
     >
       <MenuLeftRightIcon />
     </div>
