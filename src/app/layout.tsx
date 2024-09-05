2cc73ab1a39fc723a644fa72f60d229ffaed1e93
bizhermit <rsenda@bizhermit.com>
2024-09-05 13:21:11 +0900

    fix: nav cont main width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: "device-width",
+  height: "device-height",
   initialScale: 1,
-  maximumScale: 1,
 };
 
 export default function RootLayout({
