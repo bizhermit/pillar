e8904409841107654baf2b65f25123048a246842
bizhermit <rsenda@bizhermit.com>
2024-09-06 12:17:55 +0900

    feat: loading-bar

---
+++ src/components/react/elements/loading/index.tsx
@@ -0,0 +1,28 @@
+import { lazy, type HTMLAttributes } from "react";
+import { joinClassNames } from "../utilities";
+
+const LoadingBarWithMask = lazy(() => import("./bar-mask"));
+
+type LoadingOptions = {
+  mask: true;
+  position?: undefined;
+} | {
+  mask?: false | undefined;
+};
+
+type LoadingBarOptions = LoadingOptions;
+
+type LoadingBarProps = OverwriteAttrs<HTMLAttributes<HTMLElement>, LoadingBarOptions>;
+
+export const LoadingBar = ({
+  mask,
+  ...props
+}: LoadingBarProps) => {
+  if (mask) return <LoadingBarWithMask {...props} />;
+  return (
+    <div
+      {...props}
+      className={joinClassNames("loading-bar", props.className)}
+    />
+  );
+};
