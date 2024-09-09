eee4c44c5a1dc9afcef283d12439e47797d0fe01
bizhermit <rsenda@bizhermit.com>
2024-09-09 15:22:52 +0900

    feat: prisma accessor

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,8 @@
 import { $bool } from "@/data-items/bool";
 import { $num } from "@/data-items/number";
 import { $str } from "@/data-items/string";
-import { apiMethodHandler } from "@/server/next-api-handler/app-api";
+import { apiMethodHandler } from "@/server/next/app-api";
+import db from "@/server/prisma";
 
 export const GET = apiMethodHandler(async (props) => {
   const data = await props.getParams([
@@ -12,7 +13,9 @@ export const GET = apiMethodHandler(async (props) => {
   // eslint-disable-next-line no-console
   console.log(data);
   props.throwIfHasValidationError();
+  const count = await db.user.count();
   return {
     req: data,
+    count,
   };
 });
