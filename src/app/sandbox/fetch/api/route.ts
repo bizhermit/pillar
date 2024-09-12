47bbb68de2b74a5db63cec0b39d5db741fb49393
bizhermit <rsenda@bizhermit.com>
2024-09-12 16:10:06 +0900

    feat: docker-compose network

--- src/app/sandbox/fetch/api/route.ts
+++ src/app/sandbox/fetch/api/route.ts
@@ -2,6 +2,7 @@ import { formatDate } from "@/objects/date";
 import { apiMethodHandler } from "@/server/next/app-api";
 
 export const GET = apiMethodHandler(async () => {
+  // eslint-disable-next-line no-console
   console.log("[api]: /sandbox/fetch/api [GET]");
   return {
     datetime: formatDate(new Date(), "yyyy-MM-dd hh:mm:ss.SSS"),
@@ -9,6 +10,7 @@ export const GET = apiMethodHandler(async () => {
 });
 
 export const POST = apiMethodHandler(async () => {
+  // eslint-disable-next-line no-console
   console.log("[api]: /sandbox/fetch/api [POST]");
   return {
     datetime: formatDate(new Date(), "yyyy-MM-dd hh:mm:ss.SSS"),
