cc7a63faae5758bb80bc987451399991fc025022
bizhermit <rsenda@bizhermit.com>
2024-10-03 19:06:47 +0900

    feat: cookie accessor as client

---
+++ src/components/utilities/cookie.ts
@@ -0,0 +1,55 @@
+export const getCookies = () => {
+  const ret: { [v: string]: string } = {};
+  if (typeof window === "undefined") {
+    // eslint-disable-next-line no-console
+    console.warn(`faild to get cookie, not client side. [${name}]`);
+    return ret;
+  }
+  document.cookie.split(/;\s?/g).forEach(c => {
+    const [n, v] = c.split("=");
+    ret[n] = v;
+  });
+  return ret;
+};
+
+export const getCookie = (name: string) => {
+  if (typeof window === "undefined") {
+    // eslint-disable-next-line no-console
+    console.warn(`faild to get cookie, not client side. get: [${name}]`);
+    return undefined;
+  }
+  const re = new RegExp(`^${encodeURIComponent(name)}=(.+)`);
+  const v = document.cookie.split(/;\s?/g).find(c => c.match(re))?.split("=")[1];
+  return v ? decodeURIComponent(v) : v;
+};
+
+type CookieOptions = {
+  path?: PagePath;
+  domain?: string;
+  maxAge?: number;
+  expires?: string;
+  secure?: boolean;
+  httpOnly?: boolean;
+  samesite?: "Strict" | "Lax" | "None";
+};
+
+export const setCookie = (name: string, value: string, opts?: CookieOptions) => {
+  if (typeof window === "undefined") {
+    // eslint-disable-next-line no-console
+    console.warn(`faild to get cookie, not client side. set: [${name}]`);
+    return undefined;
+  }
+  const strs: Array<string> = [];
+  if (opts?.path) strs.push(`Path=${encodeURIComponent(opts.path)}`);
+  if (opts?.domain) strs.push(`Domain=${encodeURIComponent(opts.domain)}`);
+  if (opts?.expires) strs.push(`Expires=${encodeURIComponent(opts.expires)}`);
+  if (opts?.maxAge != null) strs.push(`Max-Age=${opts.maxAge}`);
+  if (opts?.secure) strs.push("Secure");
+  if (opts?.httpOnly) strs.push("HttpOnly");
+  strs.push(`SameSite=${opts?.samesite || "Lax"}`);
+  document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)};${strs.join(";")}`;
+};
+
+export const deleteCookie = (name: string) => {
+  setCookie(name, "", { maxAge: 0 });
+};
