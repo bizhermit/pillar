2fa92cf5834b344746ccce332eb09782caa75e59
bizhermit <rsenda@bizhermit.com>
2024-10-09 10:31:48 +0900

    refact: lang consts

--- src/components/i18n/consts.ts
+++ src/components/i18n/consts.ts
@@ -2,10 +2,14 @@ export const LANG_KEY = "lang";
 
 export const DEFAULT_LANG = "ja";
 
-export const LANGS = [DEFAULT_LANG, "en-US", "en"] as const;
+export const LANGS = [
+  DEFAULT_LANG,
+  "en-US",
+  "en",
+] as const;
 
 export const LANG_LABELS: { [v in typeof LANGS[number]]: string; } = {
   ja: "日本語",
   en: "English",
-  "en-US": "English (United States)",
+  "en-US": "English (US)",
 };
