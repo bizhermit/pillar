f0384aae536a96777adffc3b11b3895da46b170f
bizhermit <rsenda@bizhermit.com>
2024-10-25 19:00:28 +0900

    feat: playwright wait async components

--- playwright/context.ts
+++ playwright/context.ts
@@ -7,21 +7,57 @@ type PlaywrightContextArgs = PickPartial<PlaywrightArgs, "page">;
 let count = 0;
 
 export const getPlaywrightPageContext = (args: PlaywrightContextArgs, testInfo: TestInfo) => {
+  const waitLoading = async () => {
+    await args.page.waitForFunction(() => {
+      return document.querySelectorAll(`div[class^="loading-"]`).length === 0;
+    });
+  };
+
+  const waitImgs = async () => {
+    const imgs = await args.page.locator("img").all();
+    if (imgs.length === 0) return;
+    for await (const img of imgs) {
+      await img.waitFor({ state: "visible" });
+    }
+    await args.page.evaluate(() => {
+      return Array.from(document.images).every(img => img.complete && img.naturalHeight !== 0);
+    });
+  };
+
+  const waitLoadable = async () => {
+    await args.page.waitForFunction(() => {
+      return document.querySelectorAll(`div[data-loaded="false"]`).length === 0;
+    });
+  };
+
+  const waitShowedDialog = async () => {
+    await args.page.waitForFunction(() => {
+      return document.querySelectorAll(`dialog[open],dialog:popover-open`).length > 0;
+    });
+  };
+
   return {
-    saveSS: async (name?: string) => {
+    waitLoading,
+    waitImgs,
+    waitLoadable,
+    waitShowedDialog,
+    screenShot: async (name?: string) => {
+      await waitLoading();
+      await waitImgs();
+      await waitLoadable();
       await args.page.screenshot({
         path: `${testInfo.snapshotDir}/${testInfo.project.name || args.browserName || "default"}/${(`0000` + count++).slice(-4)}_${name?.replace(/^\//, "") || `${Date.now()}`}.png`,
         fullPage: true,
       });
     },
-    waitImgs: async () => {
-      const imgs = await args.page.locator("img").all();
-      for await (const img of imgs) {
-        await img.waitFor({ state: "visible" });
-      }
-      await args.page.evaluate(() => {
-        return Array.from(document.images).every(img => img.complete && img.naturalHeight !== 0);
-      });
+    textBox: async (name: string) => {
+      return args.page.locator(`input[type="text"][data-name="${name}"]`);
+    },
+    numberBox: async (name: string) => {
+      return args.page.locator(`input[type="text"][data-name="${name}"]`);
+    },
+    selectBox: (name: string) => {
+
     },
   };
 };
--- playwright/screen-shot.spec.ts
+++ playwright/screen-shot.spec.ts
@@ -2,15 +2,15 @@ import test from "@playwright/test";
 import { getPlaywrightPageContext } from "./context";
 
 test("test", async ({ page, browserName }, testInfo) => {
-  const { saveSS } = getPlaywrightPageContext({ page, browserName }, testInfo);
+  const { screenShot } = getPlaywrightPageContext({ page, browserName }, testInfo);
 
   await page.goto("/");
-  await saveSS();
+  await screenShot();
   await page.locator("a").filter({ hasText: "sandbox" }).click();
   await page.waitForURL("/sandbox");
   await page.waitForSelector("nav");
-  await saveSS();
+  await screenShot();
   await page.locator("a.nav-menu-item").filter({ hasText: "React Elements" }).click();
   await page.waitForURL("/sandbox/element");
-  await saveSS();
+  await screenShot();
 });
