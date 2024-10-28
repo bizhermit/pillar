302e14878fc8d0b20b1424ea84ca1a21250d4822
bizhermit <rsenda@bizhermit.com>
2024-10-28 10:13:55 +0900

    fix: sandbox set values

--- playwright/screen-shot.spec.ts
+++ playwright/screen-shot.spec.ts
@@ -20,12 +20,18 @@ test("test", async ({ page, browserName }, testInfo) => {
   await f.checkBox("sample_bool", true);
   await f.toggleSwitch("toggle", true);
   await f.checkList("check-list", ["item-2"]);
-  await f.dateBox("date", { y: 2024, m: 12, d: 31 });
+  await f.dateBox("date", { y: 2024, m: 12, d: 30 });
+  await f.dateBox("date-after", { y: 2024, m: 12, d: 31 });
+  await f.dateBox("month", { y: 2024, m: 12 });
   await f.dateBox("date-select", { y: 2024, m: 12, d: 31 });
+  await f.dateBox("month-select", { y: 2024, m: 12 });
   await f.timeBox("time", { h: 7, m: 30 });
   await f.textArea("text-area", "hoge\nfuga\npiyo");
+  await f.radioButtons("radio", "item-1");
   await f.fileChoose("file", "public/next.svg");
   await f.fileDrop("file-drop", { name: "public/vercel.svg", path: "public/vercel.svg", type: "svg" });
+  await f.textBox("credit-card", "1111222233334444");
+
   await f.submit();
   await screenShot();
 });
