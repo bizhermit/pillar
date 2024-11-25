ad7bcd07555b86dcccdfd6d2a8c9fa6751f5fe88
bizhermit <rsenda@bizhermit.com>
2024-11-25 12:25:45 +0900

    fix: next config image unoptimized flag

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 const appMode = process.env.APP_MODE;
 const outputMode = process.env.NEXT_OUTPUT;
 
+const isExport = ['dist', 'renderer'].includes(outputMode);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -10,19 +12,21 @@ const nextConfig = {
     APP_MODE: appMode,
     API_URL: process.env.API_URL || process.env.API_URL_IN_CLIENT,
   },
+  output: isExport ? "export" : undefined,
   ...(outputMode === 'dist' ? {
-    output: 'export',
     distDir: 'dist/out',
     trailingSlash: true,
   } : {}),
   ...(outputMode === 'renderer' ? {
-    output: 'export',
     distDir: '.renderer',
   } : {}),
   pageExtensions: [
     ...(appMode === 'mock' ? ['mock.tsx', 'mock.ts'] : []),
     'tsx', 'ts',
   ],
+  images: {
+    unoptimized: isExport,
+  }
 };
 
 export default nextConfig;
