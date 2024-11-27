7cde59efee5ebbb9ace9847f66f5eae82d6c23e4
bizhermit <rsenda@bizhermit.com>
2024-11-27 09:17:56 +0900

    feat: add listview sandbox page

---
+++ src/app/sandbox/listview/page.tsx
@@ -0,0 +1,14 @@
+import css from "./page.module.scss";
+
+const Page = () => {
+  return (
+    <>
+      <h1>ListView</h1>
+      <div className={css.main}>
+
+      </div>
+    </>
+  );
+};
+
+export default Page;
