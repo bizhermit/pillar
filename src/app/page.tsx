1d9abd425e89d68f369cfba4270deb2604220e62
bizhermit <rsenda@bizhermit.com>
2024-12-17 10:33:55 +0900

    fix: revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-import { redirect } from "@/server/next/navigation";
+import { langFactory } from "@/i18n/next-factory";
+import Link from "@/react/elements/link";
 
 const Page = async () => {
-  redirect("/sign-in");
-  return null;
+  const lang = await langFactory();
+
+  return (
+    <>
+      <h1>NextApp Template</h1>
+      <h2>{lang("common.halloWorld")}</h2>
+      <ul>
+        <li>
+          <Link href="/sign-in">SignIn</Link>
+        </li>
+        <li>
+          <Link href="/sandbox">sandbox</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Page;
